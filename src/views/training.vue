2ad03fc2b94f0866c7ca84c17c0b8d6240c8083b
kishitomoya <130297841+kishitomoya@users.noreply.github.com>
2023-04-16 22:01:41 +0900

    Update training.vue

--- src/views/training.vue
+++ src/views/training.vue
@@ -1,14 +1,9 @@
 <template>
-<div>
-    This is  trainingPage!
-</div>
+    <div>
+        This is trainingPage!
+    </div>
 </template>
 
-<script>
-export default {
-    name: "training.vue"
-}
-</script>
 
 <style scoped>
 
